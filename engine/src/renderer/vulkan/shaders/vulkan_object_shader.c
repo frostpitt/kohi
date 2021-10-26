242ba981a5a2c438119dd0b29b7f4cd3ee8493f1
Travis Vroman <travisvroman@gmail.com>
2021-10-26 01:10:31 -0400

    Testing a fix for ubos on some GPUs

--- engine/src/renderer/vulkan/shaders/vulkan_object_shader.c
+++ engine/src/renderer/vulkan/shaders/vulkan_object_shader.c
@@ -112,7 +112,7 @@ b8 vulkan_object_shader_create(vulkan_context* context, vulkan_object_shader* ou
     // Create uniform buffer.
     if (!vulkan_buffer_create(
             context,
-            sizeof(global_uniform_object),
+            sizeof(global_uniform_object) * 3,
             VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
             VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT | VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
             true,
@@ -173,7 +173,7 @@ void vulkan_object_shader_update_global_state(vulkan_context* context, struct vu
 
     // Configure the descriptors for the given index.
     u32 range = sizeof(global_uniform_object);
-    u64 offset = 0;
+    u64 offset = sizeof(global_uniform_object) * image_index;
 
     // Copy data to buffer
     vulkan_buffer_load_data(context, &shader->global_uniform_buffer, offset, range, 0, &shader->global_ubo);
